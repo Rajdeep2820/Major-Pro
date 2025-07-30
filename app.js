9615a99b9bd6b192bd4b3bc966e94ee47281f1c7
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-07-30 23:22:18 +0530

    Navbar and footer

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set("path" , path.join(__dirname , "views"));
 app.use(express.urlencoded(({extended : true})));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate)
+app.use(express.static(path.join(__dirname, "/public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
