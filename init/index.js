9cd0214dbe1c802111c7a80a4e246645b29a0e9a
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-11-16 11:03:15 +0530

    reiniatialized database with owner in listing.

--- init/index.js
+++ init/index.js
@@ -15,6 +15,7 @@ async function main() {
 const initDB = async () => {
   try {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj , owner : "690df149a484b264ee0c782b"}));
     await Listing.insertMany(initData.data);
     console.log("data is initialized.");
   } catch (e) {
