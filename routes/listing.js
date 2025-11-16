1df27ed518fd2a6bea2714731f845dbbd79e737e
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-11-16 11:05:47 +0530

    Correct Owner info visible below listing for anyone who is loggedin.

--- routes/listing.js
+++ routes/listing.js
@@ -44,6 +44,7 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
     // }
     // let result = listingSchema.validate();
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listings");
@@ -52,11 +53,12 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 // Show ROUTE
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews");
+    const listing = await Listing.findById(id).populate("reviews").populate("owner");
     if (!listing) {
         req.flash("error", "Listing doesn't exist!");
         return res.redirect("/listings");
     }
+    console.log(listing);
     res.render("listings/show.ejs", { listing })
 }));
 
