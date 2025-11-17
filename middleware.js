3763f330baac9c4da4d4b332fe8fe025b037c743
Rajdeep2820 <rajdeepsinghpanwar1@gmail.com>
2025-11-17 23:23:28 +0530

    3 new middlewares - 1 for listing authorization and 2 for server side validation.

--- middleware.js
+++ middleware.js
@@ -1,3 +1,7 @@
+const Listing = require("./models/listings");
+const { listingSchema , reviewSchema} = require("./schema.js");
+const ExpressError = require("./utils/ExpressError.js");
+
 module.exports.isLoggedIn = (req,res,next) => {
     console.log(req.user);
     req.session.redirecURL = req.originalUrl;
@@ -14,3 +18,35 @@ if(req.session.redirecURL){
 }
 next();
 }
+
+module.exports.isOwner = async (req,res,next) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    if(!listing.owner._id.equals(res.locals.currUser._id)){
+        req.flash("error" , "You do not have the permissions to alter the listing.");
+        res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400, error);
+    }
+    else {
+    next();
+    }
+};
+
+module.exports.validateReview = (req,res,next) => {
+    let {error} = reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400 , error);
+    }
+    else{
+        next();
+    }
+};
